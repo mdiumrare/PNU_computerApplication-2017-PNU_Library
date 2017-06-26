b6ac814962317650bdf0023b7f61cdeb98a79a4a
Daniel Kim <skaghzz@naver.com>
2017-06-26 22:04:28 +0900

    update README.md
    update README.md

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # PNU_computerApplication-2017-PNU_Library
 PNU_computerApplication-2017-PNU_Library
+## 도서 검색
+소장된 도서 검색한다.
+카테고리를 선택하고 키워드로 검색한다.
+![도서검색](./test_image/searchBook.png)
+## 대출 도서 조회
+사용자의 대출된 도서를 조회한다.
+대출도서 기간연장이 가능하다.
+![도서검색](./test_image/search_rent_book.png)
---
+++ src/QuestionBoard/index.php
@@ -0,0 +1,215 @@
+<?php
+session_start();
+$user_number = $_SESSION['user_number'];
+$user_name = $_SESSION['user_name'];
+?>
+<?php
+	require_once("../dbconfig.php");
+	
+	/* 페이징 시작 */
+	//페이지 get 변수가 있다면 받아오고, 없다면 1페이지를 보여준다.
+	if(isset($_GET['page'])) {
+		$page = $_GET['page'];
+	} else {
+		$page = 1;
+	}
+	
+	/* 검색 시작 */
+	
+	if(isset($_GET['searchColumn'])) {
+		$searchColumn = $_GET['searchColumn'];
+		$subString .= '&amp;searchColumn=' . $searchColumn;
+	}
+	if(isset($_GET['searchText'])) {
+		$searchText = $_GET['searchText'];
+		$subString .= '&amp;searchText=' . $searchText;
+	}
+	
+	if(isset($searchColumn) && isset($searchText)) {
+		$searchSql = ' where ' . $searchColumn . ' like "%' . $searchText . '%"';
+	} else {
+		$searchSql = '';
+	}
+	
+	/* 검색 끝 */
+	
+	$sql = 'select count(*) as cnt from board_free' . $searchSql;
+	$result = $db->query($sql);
+	$row = $result->fetch_assoc();
+	
+	$allPost = $row['cnt']; //전체 게시글의 수
+	
+	if(empty($allPost)) {
+		$emptyData = '<tr><td class="textCenter" colspan="5">글이 존재하지 않습니다.</td></tr>';
+	} else {
+
+		$onePage = 15; // 한 페이지에 보여줄 게시글의 수.
+		$allPage = ceil($allPost / $onePage); //전체 페이지의 수
+		
+		if($page < 1 && $page > $allPage) {
+?>
+			<script>
+				alert("존재하지 않는 페이지입니다.");
+				history.back();
+			</script>
+<?php
+			exit;
+		}
+	
+		$oneSection = 10; //한번에 보여줄 총 페이지 개수(1 ~ 10, 11 ~ 20 ...)
+		$currentSection = ceil($page / $oneSection); //현재 섹션
+		$allSection = ceil($allPage / $oneSection); //전체 섹션의 수
+		
+		$firstPage = ($currentSection * $oneSection) - ($oneSection - 1); //현재 섹션의 처음 페이지
+		
+		if($currentSection == $allSection) {
+			$lastPage = $allPage; //현재 섹션이 마지막 섹션이라면 $allPage가 마지막 페이지가 된다.
+		} else {
+			$lastPage = $currentSection * $oneSection; //현재 섹션의 마지막 페이지
+		}
+		
+		$prevPage = (($currentSection - 1) * $oneSection); //이전 페이지, 11~20일 때 이전을 누르면 10 페이지로 이동.
+		$nextPage = (($currentSection + 1) * $oneSection) - ($oneSection - 1); //다음 페이지, 11~20일 때 다음을 누르면 21 페이지로 이동.
+		
+		$paging = '<ul>'; // 페이징을 저장할 변수
+		
+		//첫 페이지가 아니라면 처음 버튼을 생성
+		if($page != 1) { 
+			$paging .= '<li class="page page_start"><a href="./index.php?page=1' . $subString . '">처음</a></li>';
+		}
+		//첫 섹션이 아니라면 이전 버튼을 생성
+		if($currentSection != 1) { 
+			$paging .= '<li class="page page_prev"><a href="./index.php?page=' . $prevPage . $subString . '">이전</a></li>';
+		}
+		
+		for($i = $firstPage; $i <= $lastPage; $i++) {
+			if($i == $page) {
+				$paging .= '<li class="page current">' . $i . '</li>';
+			} else {
+				$paging .= '<li class="page"><a href="./index.php?page=' . $i . $subString . '">' . $i . '</a></li>';
+			}
+		}
+		
+		//마지막 섹션이 아니라면 다음 버튼을 생성
+		if($currentSection != $allSection) { 
+			$paging .= '<li class="page page_next"><a href="./index.php?page=' . $nextPage . $subString . '">다음</a></li>';
+		}
+		
+		//마지막 페이지가 아니라면 끝 버튼을 생성
+		if($page != $allPage) { 
+			$paging .= '<li class="page page_end"><a href="./index.php?page=' . $allPage . $subString . '">끝</a></li>';
+		}
+		$paging .= '</ul>';
+		
+		/* 페이징 끝 */
+		
+		
+		$currentLimit = ($onePage * $page) - $onePage; //몇 번째의 글부터 가져오는지
+		$sqlLimit = ' limit ' . $currentLimit . ', ' . $onePage; //limit sql 구문
+		
+		$sql = 'select * from board_free' . $searchSql . ' order by b_no desc' . $sqlLimit; //원하는 개수만큼 가져온다. (0번째부터 20번째까지
+		$result = $db->query($sql);
+	}
+?>
+
+
+<!DOCTYPE html>
+<head>
+	   <meta name="description" content="">
+    <meta name="author" content="Dashboard">
+    <meta name="keyword" content="Dashboard, Bootstrap, Admin, Template, Theme, Responsive, Fluid, Retina">
+
+    <title>DASHGUM - Bootstrap Admin Template</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="../assets/css/bootstrap.css" rel="stylesheet">
+    <!--external css-->
+    <link href="../assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
+        
+    <!-- Custom styles for this template -->
+    <link href="../assets/css/style.css" rel="stylesheet">
+    <link href="../assets/css/style-responsive.css" rel="stylesheet">
+
+    <link href="../assets/css/table-responsive.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+<body>
+	
+		  		<div class="row mt">
+			  		<div class="col-lg-12">
+                      <div class="content-panel">
+                      <h4><i class="fa fa-angle-right"></i> Q&A 문의사항 게시판</h4>
+                      
+		<div>
+			<table class="table table-bordered table-hover">
+				
+				<thead>
+					<tr>
+						<th scope="col" class="no">번호</th>
+						<th scope="col" class="title">제목</th>
+						<th scope="col" class="author">작성자</th>
+						<th scope="col" class="date">작성일</th>
+						<th scope="col" class="hit">조회</th>
+					</tr>
+				</thead>
+				<tbody>
+						<?php
+						if(isset($emptyData)) {
+							echo $emptyData;
+						} else {
+							while($row = $result->fetch_assoc())
+							{
+								$datetime = explode(' ', $row['b_date']);
+								$date = $datetime[0];
+								$time = $datetime[1];
+								if($date == Date('Y-m-d'))
+									$row['b_date'] = $time;
+								else
+									$row['b_date'] = $date;
+						?>
+						<tr>
+							<td class="no"><?php echo $row['b_no']?></td>
+							<td class="title">
+								<a href="./view.php?bno=<?php echo $row['b_no']?>"><?php echo $row['b_title']?></a>
+							</td>
+							<td class="author"><?php echo $row['b_id']?></td>
+							<td class="date"><?php echo $row['b_date']?></td>
+							<td class="hit"><?php echo $row['b_hit']?></td>
+						</tr>
+						<?php
+							}
+						}
+						?>
+				</tbody>
+			</table>
+			<div class = "text-right">
+				<a href="./write.php" class="btnWrite btn">글쓰기</a>
+			</div>
+			<div class = "text-center">
+				<?php echo $paging ?>
+			</div>
+			<div class = "text-center">
+				<form action="./index.php" method="get">
+					<select name="searchColumn">
+						<option <?php echo $searchColumn=='b_title'?'selected="selected"':null?> value="b_title">제목</option>
+						<option <?php echo $searchColumn=='b_content'?'selected="selected"':null?> value="b_content">내용</option>
+						<option <?php echo $searchColumn=='b_id'?'selected="selected"':null?> value="b_id">작성자</option>
+					</select>
+					<input type="text" name="searchText" value="<?php echo isset($searchText)?$searchText:null?>">
+					<button type="submit">검색</button>
+				</form>
+			</div>
+		</div>
+	       
+                  </div><!-- /content-panel -->
+               </div><!-- /col-lg-4 -->			
+		  	</div><!-- /row -->
+		 
+</body>
+</html>
